481f98e67099f27d31c8742c032997e4ab49b86e
ds2e <38878730+ds2e@users.noreply.github.com>
2024-08-16 20:03:31 +0200

    change sections outlook

--- js/mainjs.js
+++ js/mainjs.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (window.innerWidth <= 767) {
         const navLinks = document.querySelectorAll('.nav-item');
         const menuToggle = document.getElementById('navbarSupportedContent');
-        const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false});
+        const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
         navLinks.forEach((l) => {
             l.addEventListener('click', () => { bsCollapse.toggle() })
         })
@@ -13,7 +13,7 @@ document.addEventListener("resize", function () {
     if (window.innerWidth <= 767) {
         const navLinks = document.querySelectorAll('.nav-item');
         const menuToggle = document.getElementById('navbarSupportedContent');
-        const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false});
+        const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
         navLinks.forEach((l) => {
             l.addEventListener('click', () => { bsCollapse.toggle() })
         })
@@ -37,4 +37,39 @@ document.addEventListener("scroll", function () {
             $('#navbarSupportedContent ul li a').eq(i).addClass('active');
         }
     });
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 500,
+    // loopAdditionalSlides: 2,
+    // loopAddBlankSlides: false,
+    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 5,
+
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 15,
+        depth: -100,
+        slideShadows: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        // when window width is >= 575px
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+
+    }
+
 });
